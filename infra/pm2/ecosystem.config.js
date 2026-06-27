5cf87d13b77f38a299826d7206325f53e4aed887
Bryan Koyundi <bryan@deeptrack.io>
2026-06-27 23:48:52 +0300

    infra: add nginx config and pm2 ecosystem file

---
+++ infra/pm2/ecosystem.config.js
@@ -0,0 +1,11 @@
+﻿module.exports = {
+  apps: [{
+    name: "gotham-enterprise",
+    script: "/home/ec2-user/Gotham-Enterprise/.next/standalone/server.js",
+    env: {
+      NODE_ENV: "production",
+      PORT: 3000,
+      HOSTNAME: "127.0.0.1"
+    }
+  }]
+}
